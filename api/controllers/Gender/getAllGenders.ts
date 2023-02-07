03343237be6d46c472e0448a6fe90867c63e4e0a
miguelbel00 <miguelabeltranp@gmail.com>
2023-02-07 12:37:52 -0500

    fix controller getallgenders

--- api/controllers/Gender/getAllGenders.ts
+++ api/controllers/Gender/getAllGenders.ts
@@ -1,13 +1,15 @@
 import axios from "axios"
-
+import type {Gender}  from "../../types"
 
 export const getAllGenders = async (_req: any, res: any) => {
 
-        try {
-             
-                 const response = await axios.get(`https://api.rawg.io/api/genres?key=${process.env.API_KEY}`)
-                
-                return res.json(response.data) 
+        const genders:Array<Gender> = []
+        try {     
+                const response = await axios.get(`https://api.rawg.io/api/genres?key=${process.env.API_KEY}`)
+                response.data.results.forEach((gender: any) => genders.push({id:gender.id,name:gender.name}) )
+
+
+                return res.json(genders) 
         } catch (error) {
             console.log(error)
         }
